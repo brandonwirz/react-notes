48fc4b7b0cd34785c3ad8b2a45e9294ee39f26e9
brandon wirz <bwirz75@gmail.com>
2023-03-30 10:33:40 -0600

    react markdown notes app

---
+++ README.md
@@ -0,0 +1,5 @@
+React Markdown Notes App Dependencies:
+Nano Id - A tiny, secure, URL-friendly, unique string ID generator for JavaScript
+React Split - The <Split /> component wraps multiple children components to create a resizeable split view screen
+React Showdown - Render markdown as React components. Render React components within the markdown
+React MDE - React Markdown Editor
---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React from "react";
+import "./style.css";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+import Split from "react-split";
+import { nanoid } from "nanoid";
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+export default function App() {
+  const [notes, setNotes] = React.useState(
+    // lazy state initialization
+    () => JSON.parse(localStorage.getItem("notes")) || []
+  );
+
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ""
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+    // console.log(notes[0].body.split("\n"));
+  }, [notes]); //dependency array runs everytime notes array changes
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  }
+
+  function updateNote(text) {
+    // rearrange notes to the top
+    setNotes((oldNotes) => {
+      const newArray = [];
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i];
+        if (oldNote.id === currentNoteId) {
+          newArray.unshift({ ...oldNote, body: text });
+        } else {
+          newArray.push(oldNote);
+        }
+      }
+      return newArray;
+    });
+  }
+  // This does not rearrange the notes
+  // function updateNote(text) {
+  //   setNotes((oldNotes) =>
+  //     oldNotes.map((oldNote) => {
+  //       return oldNote.id === currentNoteId
+  //         ? { ...oldNote, body: text }
+  //         : oldNote;
+  //     })
+  //   );
+  // }
+
+  function findCurrentNote() {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  }
+
+  return (
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
+}
